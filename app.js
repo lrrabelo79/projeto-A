be9317ccfe34a436997b65c40a0a1808d99c3831
lrrabelo79 <lrrabelo79@gmail.com>
2026-01-16 16:14:31 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const PAGES = [
+  { title:"GÊNESIS 1:1", img:"GENESIS/G1-1.png", imgFull:true },
+  { title:"GÊNESIS 1:2", img:"GENESIS/G1-2.png", imgFull:true },
+  { title:"GÊNESIS 1:3", img:"GENESIS/G1-3.png", imgFull:true },
+  { title:"GÊNESIS 1:4", img:"GENESIS/G1-4.png", imgFull:true },
+  { title:"GÊNESIS 1:5", img:"GENESIS/G1-5.png", imgFull:true },
+  { title:"GÊNESIS 1:6", img:"GENESIS/G1-6.png", imgFull:true },
+  { title:"GÊNESIS 1:7", img:"GENESIS/G1-7.png", imgFull:true },
+  { title:"GÊNESIS 1:8", img:"GENESIS/G1-8.png", imgFull:true },
+  { title:"GÊNESIS 1:9", img:"GENESIS/G1-9.png", imgFull:true },
+];
 
